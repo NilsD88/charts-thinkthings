b1b57f761bc7df49715d52545c802d9fd964a75a
Nils Devos <nils.devos@ordina.be>
2016-05-11 12:02:23 +0200

    Reset button!

--- src/main/webapp/resources/application.js
+++ src/main/webapp/resources/application.js
@@ -1,6 +1,7 @@
 var randomData;
+var value;
 
-$('#randomDataChart').highcharts({
+var chartOptions = {
   chart : {
 	height: 800,
     type : 'line',
@@ -49,15 +50,91 @@ $('#randomDataChart').highcharts({
     name : 'Data',
       data : [ ]
     } ]
-});
+};
+
+var $chart = $('#randomDataChart').highcharts(chartOptions);
 
 var socket = new SockJS('/thinkthings/random');
 var client = Stomp.over(socket);
 
+var $resetButton = $('#resetBtn');
+var resetValue;
+
+$resetButton.on('click', function(){
+	resetValue = value;
+	$chart.highcharts().destroy();
+	redrawChart();
+});
+
+function redrawChart(){
+	var $chart1 = null;
+	var chartOptions1 = {
+			  chart : {
+				height: 800,
+			    type : 'line',
+			    events : {
+			      load : function() {
+			        randomData = this.series[0];
+			      }
+			    }
+			  },
+			  title : {
+			    text : 'PIR Movement counter'
+			  },
+			  tooltip: {
+			      enabled: true
+			  },
+			  colors: ['#542791', '#542791', '#542791'],
+			  xAxis : {
+			    type : 'datetime',
+			    minRange : 60 * 60 * 1000
+			  },
+			  yAxis : {
+				allowDecimals: false,
+			    title : {
+			      text : false
+			    },
+			    gridLineWidth: 1,
+			    labels: {
+			        style: {
+			            color: 'grey'
+			        }
+			    }
+			  },
+			  legend : {
+			    enabled : false
+			  },
+			  plotOptions : {
+			    series : {
+			      threshold : 0,
+			      marker : {
+			        enabled : false
+			      }
+			    }
+			  },
+			  series : [ {
+				  lineWidth : 5,
+			    name : 'Data',
+			      data : [ ]
+			    } ]
+			};
+	randomData = chartOptions1.series[0];
+	$chart1 = $('#randomDataChart').highcharts(chartOptions1);
+} 
+
+var point;
+
 client.connect('user', 'password', function(frame) {
 
   client.subscribe("/data", function(message) {
-    var point = [ (new Date()).getTime(), parseInt(message.body) ];
+	  value = parseInt(message.body);
+	  
+	  if(typeof resetValue !== "undefined"){
+		  point = [ (new Date()).getTime(), value - resetValue];
+	  }else{
+		  point = [ (new Date()).getTime(), value ];
+	  }
+    
     randomData.addPoint(point, true);
   });
 
